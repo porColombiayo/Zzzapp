d19f115cf54a6f2880ceb7b1bc803a8ad007e7a0
Juan Camilo Salazar <juancamilosalazar@Juans-MacBook-Air-4.local>
2020-09-01 16:53:22 -0500

    Adición de tiempo y edad

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 
 var wakeTime;
-function calculateSleep() {
-    wakeTime = document.getElementById("wakeTime").value;
+var minHorasDeSueño;
+function hoursOfSleep() {
+    var años = document.getElementById("añosUsuario").value;
+    minHorasDeSueño;
+    if (años >= 18) {
+        minHorasDeSueño = 7.5;
+    }
+    else if (años >= 14 && años < 18) {
+        minHorasDeSueño = 8;
+    } else if (años >= 6 && años < 14) {
+        minHorasDeSueño = 6;
+    } else if (años >= 3 && años < 6) {
+        minHorasDeSueño = 10;
+    }
+    else if (años >= 1 && años < 3) {
+        minHorasDeSueño = 11;
+    }
 
+}
+function calculateSleep() {
+    console.log(minHorasDeSueño);
+    wakeTime = document.getElementById("wakeTime").value;
+    var wakeTimeNum = wakeTime.split(/[.:]/);
+    var hours = parseInt(wakeTimeNum[0], 10);
+    var minutes = wakeTimeNum[1] ? parseInt(wakeTimeNum[1], 10) : 0;
     console.log(wakeTime);
-    var sleepTime = ((1440.0 + ((wakeTime * 60) - 450)) - 15) / 60;
+    var timeToGetUp = hours + minutes / 60;
+    console.log(timeToGetUp);
+    var sleepTime = ((1440.0 + (((hours * 60) + minutes) - (minHorasDeSueño * 60))) - 15) / 60;
 
     var n = new Date(0, 0);
     n.setMinutes(+sleepTime * 60);
@@ -17,7 +41,4 @@ function calculateSleep() {
 </div>
     `
     document.querySelector('.salida').innerHTML = htmlS;
-}
-function sleepTime() {
-
 }
